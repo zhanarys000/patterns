7dcd2e1121e36ad5d48e5e54846a2898d3aa6a8e
zhanarys000 <221291@astanait.edu.kz>
2023-10-02 16:33:15 +0600

    choises

--- asgm1.py
+++ asgm1.py
@@ -14,36 +14,43 @@ class PaperStrategy(ChoiceStrategy):
     def make_choice(self):
         return "paper"
 
-player1_choice = input("Player 1, choose rock, scissors, or paper: ").strip().lower()
-player2_choice = input("Player 2, choose rock, scissors, or paper: ").strip().lower()
+class Context:
+    _strategy = None
 
-if player1_choice == "rock":
-    player1_strategy = RockStrategy()
-elif player1_choice == "scissors":
-    player1_strategy = ScissorsStrategy()
-else:
-    player1_strategy = PaperStrategy()
+    def setStrategy(self, choice):
+        if choice == "rock":
+            self._strategy = RockStrategy()
+        elif choice == "scissors":
+            self._strategy = ScissorsStrategy()
+        else:
+            self._strategy = PaperStrategy()
 
-if player2_choice == "rock":
-    player2_strategy = RockStrategy()
-elif player2_choice == "scissors":
-    player2_strategy = ScissorsStrategy()
-else:
-    player2_strategy = PaperStrategy()
+    def executeStrategy(self):
+        return self._strategy.make_choice()
+    
 
-choice1 = player1_strategy.make_choice()
-choice2 = player2_strategy.make_choice()
 
-print(f"Player 1 chose: {choice1}")
-print(f"Player 2 chose: {choice2}")
+class main():
+    a = Context()
+    player1_choice = input("Player 1, choose rock, scissors, or paper: ").strip().lower()
+    player2_choice = input("Player 2, choose rock, scissors, or paper: ").strip().lower()
 
-if choice1 == choice2:
-    print("Draw!")
-elif (
-    (choice1 == "rock" and choice2 == "scissors")
-    or (choice1 == "scissors" and choice2 == "paper")
-    or (choice1 == "paper" and choice2 == "rock")
-):
-    print("Player 1 wins!")
-else:
-    print("Player 2 wins!")
+    a.setStrategy(player1_choice)
+    choice1 = a.executeStrategy()
+    print(f"Player 1 chose: {choice1}")
+
+
+    a.setStrategy(player2_choice)
+    choice2 = a.executeStrategy()
+    print(f"Player 2 chose: {choice2}")
+
+    if choice1 == choice2:
+        print("Draw!")
+    elif (
+        (choice1 == "rock" and choice2 == "scissors")
+        or (choice1 == "scissors" and choice2 == "paper")
+        or (choice1 == "paper" and choice2 == "rock")
+    ):
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
